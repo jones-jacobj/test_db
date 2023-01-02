aaaeda8c81f0a79486f23b78d054a2ea5fab8007
Jacob Jones <jacob.j.jones01@gmail.com>
2023-01-02 16:58:16 -0600

    Finished Where exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,4 +1,9 @@
 use employees;
 SELECT * from employees where first_name IN ('Irena', 'Vidya','Maya');
+SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
+SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya' AND gender = 'M';
 SELECT * from employees WHERE last_name like 'e%';
 SELECT * FROM employees WHERE last_name LIKE ('%q%');
+SELECT * from employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
+SELECT * FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e';
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
